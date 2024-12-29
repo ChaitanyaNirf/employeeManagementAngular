9fe595a2045652600f358e989120e43e7aaad5d5
chaitanyaNirf <chaitunirfarake@gmail.com>
2024-12-30 01:18:51 +0530

    practice: interpolation + dataBinding + API integration for roles

---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,70 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule, } from '@angular/forms';
+import { IRole } from '../../model/interface/roles';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-roles',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css'
+})
+
+export class RolesComponent implements OnInit {
+
+  roleList: IRole [] = [];
+  http = inject(HttpClient);
+  
+  // old way
+  // constructor(private http: HttpClient){
+
+  // }
+
+  ngOnInit(): void {
+    //ngOnInit is a lifecycle hook that is called after Angular hs initialized the component's data bound properties
+    // alert("Rendered roles");
+
+    this.getAllRoles();
+  }
+
+  getAllRoles() {
+    this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res:any) => {
+      this.roleList = res.data;
+    })
+  }
+
+  
+}
+
+//Practice 
+// export class RolesComponent {
+  // private str: string;
+  // firstName: string = "Chaitanya Nirfarake";
+  // version: string = "1.0.0";
+  // inputType: string = "checkbox";
+  // selectedState: string = '';
+  //  msg: string = 'Default Message';
+
+  // constructor() {
+  //   this.str = this.firstName;
+  // }
+
+  // public showCurrentDate() {
+  //   try {
+  //     console.log("Hello");
+  //     return new Date();
+  //   } catch (error) {
+  //     console.log(error);
+  //     throw error;
+  //   }
+  // }
+
+  // public showWelcomeAlert(){
+  //   alert("Welcome to my project"); 
+  // }
+
+  // public showMessage(message: string){
+  //   alert(message);
+  // }
+// }
