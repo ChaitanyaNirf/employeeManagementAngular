b706d44f0f7f345f4d69be01dd79d2721949b30c
chaitanyaNirf <chaitunirfarake@gmail.com>
2025-01-03 23:14:26 +0530

    added new component : clientProject

---
+++ src/app/components/client-project/client-project.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-client-project',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './client-project.component.html',
+  styleUrl: './client-project.component.css'
+})
+export class ClientProjectComponent {
+
+}
--- src/app/services/client/client.service.ts
+++ src/app/services/client/client.service.ts
@@ -10,7 +10,7 @@ import { environment } from '../../../environments/environment';
 })
 export class ClientService {
 
-  constructor(private httpclient: HttpClient) {
+  constructor(private readonly httpclient: HttpClient) {
       
   }
 
