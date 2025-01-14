442576c0415f1d78457d00941624006ef818f40d
chaitanyaNirf <chaitunirfarake@gmail.com>
2025-01-15 01:05:56 +0530

    get client and employee data for reactive form

--- src/app/components/client-project/client-project.component.ts
+++ src/app/components/client-project/client-project.component.ts
@@ -1,13 +1,73 @@
 import { CommonModule } from '@angular/common';
-import { Component, NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, NgModule, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EmployeeService } from '../../services/employee/employee.service';
+import { HttpClient } from '@angular/common/http';
+import { IEmployee } from '../../model/interface/employee';
+import { IApiResponse } from '../../model/interface/apiResponse';
+import { ClientService } from '../../services/client/client.service';
+import { Client } from '../../model/class/Client';
 
 @Component({
   selector: 'app-client-project',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './client-project.component.html',
   styleUrl: './client-project.component.css'
 })
-export class ClientProjectComponent {
+export class ClientProjectComponent implements OnInit {
 
+  // private readonly employeeService;
+  // private readonly clientService;
+  employeeList: IEmployee[] = [];
+  clientList: Client[] = [];
+  //can create an constructor and pass it as well no issues
+  httpClient = inject(HttpClient);
+  employeeService = inject(EmployeeService);
+  clientService = inject(ClientService);
+
+  // constructor() {
+  //   this.employeeService = new EmployeeService(this.httpClient);
+  //   this.clientService = new ClientService(this.httpClient);
+  // }
+
+
+  ngOnInit(): void {
+    try {
+      this.getAllEmployees();
+      this.getAllCLients();
+    } catch (error) {
+      console.log("Failed to load data");
+    }
+  }
+
+  getAllEmployees() {
+    //load all clients
+    this.employeeService.getAllEmployees().subscribe((res: IApiResponse) => {
+      this.employeeList = res.data;
+    })
+  }
+
+  getAllCLients() {
+    //load all clients
+    this.clientService.getAllClient().subscribe((res: IApiResponse) => {
+      this.clientList = res.data;
+    })
+  }
+
+  //Reactive form
+  projectForm: FormGroup = new FormGroup({
+    clientProjectId: new FormControl(0),
+    projectName: new FormControl("New Project"),
+    startDate: new FormControl(new Date()),
+    expectedEndDate: new FormControl(new Date()),
+    leadByEmpId: new FormControl(0),
+    completeDate: new FormControl(new Date()),
+    contactPerson: new FormControl(""),
+    contactPersonContactNo: new FormControl(""),
+    totalEmpWorking: new FormControl(0),
+    projectCost: new FormControl(0),
+    projectDetails: new FormControl(""),
+    contactPersonEmailId: new FormControl(""),
+    clientId: new FormControl(0)
+  });
 }
---
+++ src/app/model/interface/employee.ts
@@ -0,0 +1,9 @@
+export interface IEmployee {
+    empName: string
+    empId: number
+    empCode: string
+    empEmailId: string
+    empDesignation: string
+    role: string
+  }
+  
