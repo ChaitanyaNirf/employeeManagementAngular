135c95beb418d2f0c13b11920d28b5f604f1645d
chaitanyaNirf <chaitunirfarake@gmail.com>
2025-01-20 17:18:22 +0530

    fixed clientProjectForm reset + refactored code

---
+++ src/constants/Constants.ts
@@ -0,0 +1,10 @@
+export const Constants = {
+    API_METHOD : {
+        GET_ALL_CLIENTS : "GetAllClients",
+        ADD_UPDATE_CLIENT : "AddUpdateClient",
+        DELETE_CLIENT_BY_CLIENT_ID : "DeleteClientByClientId?clientId=",
+        ADD_UPDATE_CLIENT_PROJECT : "AddUpdateClientProject",
+        GET_ALL_CLIENT_PROJECTS : "GetAllClientProjects",
+        GET_ALL_EMPLOYEES : "GetAllEmployee"
+    }
+}
